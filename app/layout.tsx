67ed755b54aa93ecd7dd1605b0de236b7f3eb31a
NicoBrnr <nico25.brunner@gmail.com>
2026-03-09 15:14:14 +0100

    Add Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Colorisation Photos',
-  description: 'Colorisez vos photos en noir et blanc grâce à l\'intelligence artificielle',
+  description:
+    "Colorisez vos photos en noir et blanc grâce à l'intelligence artificielle",
 };
 
 export default function RootLayout({
@@ -11,5 +13,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <html lang="fr">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PRP2PCCW');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PRP2PCCW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
+    </html>
+  );
 }
